5d71b3bf8c89503cfeeb670d4a0d3d7406db0cd9
yfjelley <yfjelley@gmail.com>
2021-11-19 00:51:01 +0800

    xx

--- auto_trader.py
+++ auto_trader.py
@@ -8,8 +8,6 @@ import time
 lots = '1000|10000'
 loop = "3|10"
 
-api_key = "516ab98d-271b-4499-b4ee-eb1772cf3773"
-secret_key = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAK/qIg4o0Rl8MLcEX68DzuEdC+jIkjEVLJtrzsW75myJKYdmDPK8mp0m0DZGwHWGDfVxYvARuAdaXeJguHSPFcXHMSirPQ1ZsnTFd1NxqoVhmpOy56iXHd4BjTuybQ4U1v9BauyxZXZFeZSBuqUAsDo9JxMsVVrc88uAggtzEPddAgMBAAECgYAo+37blZ7BNTGUMayo9VYpE79GiBOm46v0uXT+k/vmpT3LkXbKxi2vFu/C9VC5EHYIDFZkX3xkGiVtK+NNJFiJytbXPHCZsJXS1kI2wZ2CpmPjxmLQZpdnV71VJMeURQRsCIEeYFggsI3k2QR3u+SSLAiCfow5CIOFV2JlY/KYgQJBAPpXIH6Ei4rNXuhHJMN4tVcA9FwxMPmxRFi+9eRHJr3stlWsPZdn1lg3fl0g6y30tyoOwfnXHDB1yniOqI6Ie5UCQQCz5EIzbijbCtrGgGENOds+VZQqzn+BZJYvqz+IUVis0XOx6aBjlv4lfDYV9SCiCSR/f+U7os3aOTpTaDTYMVqpAkAjU2giclG+pHxgCqoFa2Mrg9b3q3ldwsYCP/Ay5ldxNZYFQOjwFJcKm8oZGiwVsBKovKxitRglPnnzyS2/70KBAkA7NQtc5groXSA4aRSIR9yTHZOQqzpoGfUcZ16XvT5UUvOjQOObI50uNT2P6If/DMdId425HRJnmqJJxWhvJ39RAkB7g7p4xCPIohtIFptWWtIpDaMTMA6COCH3CECaJLiLZ4zrLw54+/DDyPAfCeUITebDlXKxQ5S9Biqqmzgl2hP2"
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(filename)s - %(funcName)s - [%(levelname)s] - %(message)s',
